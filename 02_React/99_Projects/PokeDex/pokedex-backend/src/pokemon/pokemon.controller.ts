79070212c9a28ada7cef134b99e7a09260964030
Boncea Șerban-Andrei <bonceaandrei99@gmail.com>
2025-02-11 14:34:23 +0200

    Improve Backend Logic
    - backend application now pre-loads the Pokémon data
    - only the necessary Pokémon data is loaded and cached in-memory

--- 02_React/99_Projects/PokeDex/pokedex-backend/src/pokemon/pokemon.controller.ts
+++ 02_React/99_Projects/PokeDex/pokedex-backend/src/pokemon/pokemon.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
-  @Get('all')
-  getPokemonList() {
-    return this.pokemonService.getPokemonList();
+  @Get('')
+  getAllPokemon() {
+  return this.pokemonService.getAllPokemon();
   }
-
- /*  @Get(':id')
-  getPokemonById(@Param('id') id: string) {
-    return this.pokemonService.getPokemonById(+id);
-  } */
 }
